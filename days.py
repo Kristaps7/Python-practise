cbe9dcf5d01e9d1dea4d41d71070efecdfe9fc40
Kristaps <kfeikners24@gmail.com>
2022-02-06 22:57:32 +0200

    Initial Commit

---
+++ days.py
@@ -0,0 +1,23 @@
+d = 6
+
+if d in (1,2,3,4,5): # A
+    print('Working day')
+else:
+    print('Holiday')
+    
+if d in (6,7): # B
+    print('Holiday')
+else:
+    print('Working day')
+    
+if d>=1 and d<=5: # C
+    print('Working day')
+else:
+    print('Holiday')
+    
+if d<=5: # D
+    print('Working day')
+else:
+    print('Holiday')
+    
+        
